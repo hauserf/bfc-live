ece9234f80a40f7d775e93b32b47ec5d9c1b44c5
Frank Hauser <frank@soccerbeyond.com>
2017-07-17 17:58:02 -0400

    added react router to footer menu

---
+++ src/components/settings.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+
+export default class Settings extends Component {
+
+  render(){
+
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 "></div>
+          <div className="col-sm-4 bfc-live">
+            <div>
+              <h1>Settings</h1>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+}

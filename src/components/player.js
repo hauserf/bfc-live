9ed122136667c370db0fd3129b3ff1665edc24c7
Stephen Tapia Jr <StephenTANM@yahoo.com>
2017-07-17 10:03:51 -0700

    IF THEN
    filter players by active states

--- src/components/player.js
+++ src/components/player.js
@@ -3,16 +3,43 @@ import React, { Component } from 'react';
 
 class Player extends Component {
 
-  movePlayer(e) {
-    this.props.switchArrays();
+  subPlayerIn(index) {
+    console.log("sub In")
+    this.props.subPlayerIn(index);
+  }
+
+  subPlayerOut(index) {
+    console.log("sub Out")
+    this.props.subPlayerOut(index);
+  }
+  componentDidMount(){
+    console.log(this.props.currentActive);
   }
 
   render(){
+    // console.log(this.props.roster[this.props.index].playerActive, this.props.roster[this.props.index].playerActive)
     return (
-      <div className="roster" onClick={this.movePlayer.bind(this)}>
+      <div
+        className="roster"
+        onClick={this.subPlayerIn.bind(this, this.props.index)}
+        //   /// condition ??? // on click event needs to change once player has been moved into the other array
+        //   ?  this.subPlayerIn.bind(this, this.props.index)
+        //   :  this.subPlayerOut.bind(this, this.props.index)
+        // }
+        >
+
         <div className="player">
           {this.props.name}
+          {this.props.currentActive}
         </div>
+        <div className="player-minutes">
+          {
+            // this.props.roster[this.props.index].playerActive === false
+            // ? "SubIn"
+            // : "SubOut"
+          }
+        </div>
+
         <div className="player-minutes">
           00:00
         </div>
--- src/components/roster.js
+++ src/components/roster.js
@@ -5,25 +5,90 @@ import { PlayerStats } from '../data/player-stats';
 
 export default class Roster extends Component {
   constructor(){
-    super();
+    super(...arguments);
     this.state = {
-      startingXI: [],
-      roster: [],
+      playersPlaying: [],
+      playersNotPlaying:[],
+      roster: PlayerStats
     }
   }
 
-  movePlayer(){
-    const startingEleven = this.state.startingXI;
-    //   // move player from array "roster" to array "startingXI"
-    //   // step two: be able to move the players back and forth by simply clicking on the player div
-    //   // if player is in array roster > onClick moves him to array "startingXI" and the other way around
-    this.setState({startingXI: startingEleven });
+  togglePlayer(id){
+    const roster = this.state.roster;
+    const playerToMove = this.state.roster.splice(id, 1);
+    const playersOnField = playersPlaying.concat(playerToMove);
+    console.log(playerToMove);
+    const playersPlaying = this.state.playersPlaying;
+    const playersNotPlaying= this.state.playersNotPlaying;
+
+    if(playersOnField[0].playerActive){
+      playersPlaying.concat(playerToMove);
+    } else{
+      const playersNotOnField = playersNotPlaying.concat(playerToMove);
+    }
   }
+
+  subPlayerIn(id){
+    // onClick event = click on player in "Subs" >> move him into array "playersPlaying" ("On the field")
+
+    const roster = this.state.roster;
+    const playerToMove = this.state.roster.splice(id, 1);
+    console.log(playerToMove);
+
+    const playersPlaying = this.state.playersPlaying;
+    const playersOnField = playersPlaying.concat(playerToMove);
+
+    this.setState({ roster });
+
+    this.setState({ playersPlaying: playersOnField });
+    //playersOnField[playersOnField.length - 1].playerActive = true;
+
+    console.log(
+      playersOnField, "playersOnField",
+      playersOnField.length, "playersOnField.length",
+      playersOnField[0].playerActive, "playersOnField",
+      this.state.playersPlaying, "player [playing]",
+      this.state.roster[id].id, "roster id"
+    );
+  }
+
+  subPlayerOut(id) {
+    const playersPlaying = this.state.playersPlaying;
+    const playerToMove = playersPlaying.splice(id, 1);
+    this.setState({ playersPlaying });
+    const roster = this.state.roster;
+    const rosterList = roster.concat(playerToMove);
+    this.setState({ roster: rosterList });
+    rosterList[rosterList.length - 1].playerActive = false;
+    }
+
 
 
   render(){
+    // map/list players from data/player.stats.js in "Subs"
+    const rosterList = PlayerStats.map((player, id)  =>
+        <Player
+          name={player.firstName}
+          key={id}
+          index={id}
+          subPlayerIn={this.subPlayerIn.bind(this)}
+          roster={this.state.roster}
+        />)
 
-    const roster = PlayerStats.map((player, id)  => <Player name={player.firstName} key={id} switchArrays={this.movePlayer.bind(this)}/>)
+      // map/list players in "On the field" (as in currently playing)
+      const playersPlaying = this.state.playersPlaying;
+      const playersOnField = playersPlaying.map((player, id)  =>
+        <div key={id}>
+          <Player
+            name={player.firstName}
+            key={id}
+            index={id}
+            subPlayerOut={this.subPlayerOut.bind(this)}
+            roster={this.state.roster}
+            playerXYZIsPlaying={this.state.playersPlaying[id].id}
+            currentActive={player.playerActive}
+          />
+        </div>)
 
     return(
       <div className="container">
@@ -31,12 +96,10 @@ export default class Roster extends Component {
           <div className="col-md-4 "></div>
           <div className="col-sm-4 bfc-live">
             <div>
-              Starting XI
-              ...coming soon
-              {this.state.startingXI}
-
-              Roster
-              {roster}
+              <h1>On the field</h1>
+              {playersOnField}
+              <h1>Subs</h1>
+              {rosterList}
             </div>
           </div>
         </div>
